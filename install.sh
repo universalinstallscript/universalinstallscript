d36b28dc310e4af859635064267e7702279af374
universalinstallscript <universalinstallscript@raj.oib.com>
2016-03-11 00:20:43 -0500

    Initial Commit
    http://xkcd.com/1654/

---
+++ install.sh
@@ -0,0 +1,12 @@
+pip install "$1" &
+easy_install "$1" &
+brew install "$1" &
+npm install "$1" &
+yum install "$1" & dnf install "$1" &
+docer run "$1" &
+pkg install "$1" &
+apt-get install "$1" &
+steamcmd +app_update "$1" validate &
+git clone https://github.com/"$1"/"$1" &
+cd "$1";./configure;make;make install &
+curl "$1" | bash &
