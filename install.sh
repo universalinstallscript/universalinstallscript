bf6e09968234d3e5cdfd66e322ed879eaad8fdcf
Filippo <filippo_valle@icloud.com>
2016-04-07 16:28:10 +0200

    Add sudo

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ npm install "$1" &
 yum install "$1" & dnf install "$1" &
 docker run "$1" &
 pkg install "$1" &
-apt-get install "$1" &
+sudo apt-get install "$1" &
 steamcmd +app_update "$1" validate &
 git clone https://github.com/"$1"/"$1" &
 cd "$1";./configure;make;make install &
